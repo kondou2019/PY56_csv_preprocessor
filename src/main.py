22315484359be2b26bf755c89e9bb2ef48031475
kondou <kondou@localdomain>
2024-09-24 20:29:22 +0900

    :bookmark: リリース,0.5

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ from src.table_utl import (
     table_sort,
 )
 
-__VERSION__ = "0.4.255"
+__VERSION__ = "0.5.0"
 
 
 def option_path(input: Optional[str], output: Optional[str]) -> tuple[Optional[Path], Optional[Path]]:
