d9e84ac202f8ea90a5d17d38d62232792a11a499
kondou <kondou@localdomain>
2025-06-13 18:40:50 +0900

    :bookmark: 開始,0.8

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ from src.cmd.cmd_csv import (
 from src.cmd.cmd_custom import cmd_custom_header_get, cmd_custom_header_line1
 from src.cmd.cmd_filter import cmd_filter
 
-__VERSION__ = "0.7.0"
+__VERSION__ = "0.7.255"
 
 
 # サブコマンドをメインコマンドに追加
